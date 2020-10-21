6b6c805338ecc83c9621cf5fc6b402dc59604ff0
Zukron Alviandy <zukronalviandy@gmail.com>
2020-10-21 15:37:16 +0700

    add nilai-mahasiswa endpoint

--- json/main.go
+++ json/main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	m "universitas/json/mahasiswa"
-	mK "universitas/json/mata-kuliah"
-	n "universitas/json/nilai"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"gopkg.in/yaml.v2"
 )
 
-type Config struct {
+var db *sql.DB
+
+type config struct {
 	Connection struct {
 		Host     string `yaml:"host"`
 		Port     string `yaml:"port"`
@@ -25,50 +25,141 @@ type Config struct {
 	} `yaml:"connection"`
 }
 
+type response struct {
+	StatusCode int         `json:"status_code"`
+	Message    string      `json:"message"`
+	Mahasiswa  []mahasiswa `json:"mahasiswa"`
+}
+
+type mahasiswa struct {
+	IDMahasiswa string `json:"id_mahasiswa"`
+	Nama        string `json:"nama"`
+	Alamat      struct {
+		Jalan     string `json:"jalan"`
+		Kelurahan string `json:"kelurahan"`
+		Kecamatan string `json:"kecamatan"`
+		Kabupaten string `json:"kabupaten"`
+		Provinsi  string `json:"provinsi"`
+	} `json:"alamat"`
+	Fakultas    string        `json:"fakultas"`
+	Jurusan     string        `json:"jurusan"`
+	NilaiDetail []nilaiDetail `json:"nilai_detail"`
+}
+
+type nilaiDetail struct {
+	IDMahasiswa  string  `json:"id_mahasiswa"`
+	IDMataKuliah string  `json:"id_mata_kuliah"`
+	MataKuliah   string  `json:"mata_kuliah"`
+	Nilai        float32 `json:"nilai"`
+	Semester     int8    `json:"semester"`
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello")
 }
 
+func nilaiMahasiswa(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "GET" {
+		var response response
+		var params = mux.Vars(r)
+		var paramIdMahasiswa = params["idMahasiswa"]
+		var paramIdMataKuliah = params["idMataKuliah"]
+		var err error
+
+		var sqlMahasiswa string
+		var rowsMahasiswa *sql.Rows
+
+		var sqlNilai string
+		var rowsNilai *sql.Rows
+
+		// memilih jika ada parameter atau tidak
+		if paramIdMahasiswa == "" {
+			sqlMahasiswa = "SELECT * FROM mahasiswa"
+			rowsMahasiswa, err = db.Query(sqlMahasiswa)
+		} else {
+			sqlMahasiswa = "SELECT * FROM mahasiswa WHERE id_mahasiswa = ?"
+			rowsMahasiswa, err = db.Query(sqlMahasiswa, params["idMahasiswa"])
+		}
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			response.StatusCode = http.StatusInternalServerError
+			response.Message = "No connection into database"
+		} else {
+			defer rowsMahasiswa.Close()
+
+			w.WriteHeader(http.StatusOK)
+			response.StatusCode = http.StatusOK
+			response.Message = "Enjoy your result"
+
+			// mahasiswa
+			for rowsMahasiswa.Next() {
+				var mahasiswa mahasiswa
+
+				rowsMahasiswa.Scan(
+					&mahasiswa.IDMahasiswa, &mahasiswa.Nama, &mahasiswa.Alamat.Jalan,
+					&mahasiswa.Alamat.Kelurahan, &mahasiswa.Alamat.Kecamatan, &mahasiswa.Alamat.Kabupaten,
+					&mahasiswa.Alamat.Provinsi, &mahasiswa.Fakultas, &mahasiswa.Jurusan,
+				)
+
+				// mengecek jika ada param idMataKuliah
+				if paramIdMataKuliah == "" {
+					sqlNilai =
+						`SELECT id_mahasiswa, id_mata_kuliah, nilai, semester
+					FROM mahasiswa JOIN nilai USING(id_mahasiswa) WHERE id_mahasiswa = ?`
+					rowsNilai, _ = db.Query(sqlNilai, mahasiswa.IDMahasiswa)
+				} else {
+					sqlNilai =
+						`SELECT id_mahasiswa, id_mata_kuliah, nilai, semester
+					FROM mahasiswa JOIN nilai USING(id_mahasiswa) WHERE id_mahasiswa = ? AND id_mata_kuliah = ?`
+					rowsNilai, _ = db.Query(sqlNilai, mahasiswa.IDMahasiswa, params["idMataKuliah"])
+				}
+
+				// nilai
+				for rowsNilai.Next() {
+					var nilaiDetail nilaiDetail
+
+					rowsNilai.Scan(
+						&nilaiDetail.IDMahasiswa, &nilaiDetail.IDMataKuliah,
+						&nilaiDetail.Nilai, &nilaiDetail.Semester,
+					)
+
+					sqlMataKuliah :=
+						`SELECT nama
+						FROM mata_kuliah JOIN nilai USING(id_mata_kuliah) WHERE id_mata_kuliah = ?`
+					rowsMataKuliah, _ := db.Query(sqlMataKuliah, nilaiDetail.IDMataKuliah)
+
+					// mata kuliah
+					if rowsMataKuliah.Next() {
+						rowsMataKuliah.Scan(&nilaiDetail.MataKuliah)
+					}
+
+					mahasiswa.NilaiDetail = append(mahasiswa.NilaiDetail, nilaiDetail)
+				}
+
+				response.Mahasiswa = append(response.Mahasiswa, mahasiswa)
+			}
+		}
+
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
 func main() {
 	// database
-	db := getConnection()
+	db = getConnection()
 	defer db.Close()
 
 	// init router
 	router := mux.NewRouter()
 
-	// index
+	// router url
 	router.HandleFunc("/", index).Methods("GET")
-
-	// init objek mahasiswa
-	mahasiswa := m.Mahasiswa{DB: db}
-
-	// route mahasiswa
-	router.HandleFunc("/mahasiswa", mahasiswa.GetAllMahasiswa).Methods("GET")
-	router.HandleFunc("/mahasiswa/{id}", mahasiswa.GetMahasiswa).Methods("GET")
-	router.HandleFunc("/mahasiswa/{id}", mahasiswa.UpdateMahasiswa).Methods("PUT")
-	router.HandleFunc("/mahasiswa/{id}", mahasiswa.DeleteMahasiswa).Methods("DELETE")
-	router.HandleFunc("/mahasiswa", mahasiswa.InsertMahasiswa).Methods("POST")
-
-	// init object nilai
-	nilai := n.Nilai{DB: db}
-
-	// route nilai
-	router.HandleFunc("/nilai", nilai.GetAllNilai).Methods("GET")
-	router.HandleFunc("/nilai/{id}", nilai.GetNilai).Methods("GET")
-	router.HandleFunc("/nilai/{id}", nilai.UpdateNilai).Methods("PUT")
-	router.HandleFunc("/nilai/{id}", nilai.DeleteNilai).Methods("DELETE")
-	router.HandleFunc("/nilai", nilai.InsertNilai).Methods("POST")
-
-	// init object mata kuliah
-	mataKuliah := mK.MataKuliah{DB: db}
-
-	// route mata kuliah
-	router.HandleFunc("/mata-kuliah", mataKuliah.GetAllMataKuliah).Methods("GET")
-	router.HandleFunc("/mata-kuliah/{id}", mataKuliah.GetMataKuliah).Methods("GET")
-	router.HandleFunc("/mata-kuliah/{id}", mataKuliah.UpdateMataKuliah).Methods("PUT")
-	router.HandleFunc("/mata-kuliah/{id}", mataKuliah.DeleteMataKuliah).Methods("DELETE")
-	router.HandleFunc("/mata-kuliah", mataKuliah.InsertMataKuliah).Methods("POST")
+	router.HandleFunc("/nilai-mahasiswa", nilaiMahasiswa).Methods("GET")
+	router.HandleFunc("/nilai-mahasiswa/{idMahasiswa}", nilaiMahasiswa).Methods("GET")
+	router.HandleFunc("/nilai-mahasiswa/{idMahasiswa}/{idMataKuliah}", nilaiMahasiswa).Methods("GET")
 
 	// start server with port 8080
 	log.Fatal(http.ListenAndServe(":8000", router))
@@ -78,7 +169,7 @@ func getConnection() *sql.DB {
 	file, err := ioutil.ReadFile("../config.yml")
 	checkErr(err)
 
-	var config Config
+	var config config
 	err = yaml.Unmarshal(file, &config)
 	checkErr(err)
 
--- xml/main.go
+++ xml/main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"database/sql"
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	m "universitas/xml/mahasiswa"
-	mK "universitas/xml/mata-kuliah"
-	n "universitas/xml/nilai"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"gopkg.in/yaml.v2"
 )
 
-type Config struct {
+var db *sql.DB
+
+type config struct {
 	Connection struct {
 		Host     string `yaml:"host"`
 		Port     string `yaml:"port"`
@@ -25,50 +25,152 @@ type Config struct {
 	} `yaml:"connection"`
 }
 
+type response struct {
+	XMLName    xml.Name `xml:"responses"`
+	StatusCode int      `xml:"status_code"`
+	Message    string   `xml:"message"`
+	Mahasiswa  struct {
+		XMLName       xml.Name    `xml:"mahasiswa_list"`
+		MahasiswaList []mahasiswa `xml:"mahasiswa"`
+	}
+}
+
+type mahasiswa struct {
+	IDMahasiswa string `xml:"id_mahasiswa,attr"`
+	Nama        string `xml:"nama"`
+	Alamat      struct {
+		XMLName   xml.Name `xml:"alamat"`
+		Jalan     string   `xml:"jalan"`
+		Kelurahan string   `xml:"kelurahan"`
+		Kecamatan string   `xml:"kecamatan"`
+		Kabupaten string   `xml:"kabupaten"`
+		Provinsi  string   `xml:"provinsi"`
+	}
+	Fakultas    string `xml:"fakultas"`
+	Jurusan     string `xml:"jurusan"`
+	NilaiDetail struct {
+		XMLName         xml.Name      `xml:"nilai_detail"`
+		NilaiDetailList []nilaiDetail `xml:"nilai"`
+	}
+}
+
+type nilaiDetail struct {
+	IDMahasiswa  string  `xml:"id_mahasiswa,attr"`
+	IDMataKuliah string  `xml:"id_mata_kuliah,attr"`
+	MataKuliah   string  `xml:"mata_kuliah"`
+	Nilai        float32 `xml:"nilai"`
+	Semester     int8    `xml:"semester"`
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello")
 }
 
+func nilaiMahasiswa(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/xml")
+
+	if r.Method == "GET" {
+		var response response
+		var params = mux.Vars(r)
+		var paramIdMahasiswa = params["idMahasiswa"]
+		var paramIdMataKuliah = params["idMataKuliah"]
+		var err error
+
+		var sqlMahasiswa string
+		var rowsMahasiswa *sql.Rows
+
+		var sqlNilai string
+		var rowsNilai *sql.Rows
+
+		// memilih jika ada parameter atau tidak
+		if paramIdMahasiswa == "" {
+			sqlMahasiswa = "SELECT * FROM mahasiswa"
+			rowsMahasiswa, err = db.Query(sqlMahasiswa)
+		} else {
+			sqlMahasiswa = "SELECT * FROM mahasiswa WHERE id_mahasiswa = ?"
+			rowsMahasiswa, err = db.Query(sqlMahasiswa, params["idMahasiswa"])
+		}
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			response.StatusCode = http.StatusInternalServerError
+			response.Message = "No connection into database"
+		} else {
+			defer rowsMahasiswa.Close()
+
+			w.WriteHeader(http.StatusOK)
+			response.StatusCode = http.StatusOK
+			response.Message = "Enjoy your result"
+
+			// mahasiswa
+			for rowsMahasiswa.Next() {
+				var mahasiswa mahasiswa
+
+				rowsMahasiswa.Scan(
+					&mahasiswa.IDMahasiswa, &mahasiswa.Nama, &mahasiswa.Alamat.Jalan,
+					&mahasiswa.Alamat.Kelurahan, &mahasiswa.Alamat.Kecamatan, &mahasiswa.Alamat.Kabupaten,
+					&mahasiswa.Alamat.Provinsi, &mahasiswa.Fakultas, &mahasiswa.Jurusan,
+				)
+
+				// mengecek jika ada param idMataKuliah
+				if paramIdMataKuliah == "" {
+					sqlNilai =
+						`SELECT id_mahasiswa, id_mata_kuliah, nilai, semester
+					FROM mahasiswa JOIN nilai USING(id_mahasiswa) WHERE id_mahasiswa = ?`
+					rowsNilai, _ = db.Query(sqlNilai, mahasiswa.IDMahasiswa)
+				} else {
+					sqlNilai =
+						`SELECT id_mahasiswa, id_mata_kuliah, nilai, semester
+					FROM mahasiswa JOIN nilai USING(id_mahasiswa) WHERE id_mahasiswa = ? AND id_mata_kuliah = ?`
+					rowsNilai, _ = db.Query(sqlNilai, mahasiswa.IDMahasiswa, params["idMataKuliah"])
+				}
+
+				// nilai
+				for rowsNilai.Next() {
+					var nilaiDetail nilaiDetail
+
+					rowsNilai.Scan(
+						&nilaiDetail.IDMahasiswa, &nilaiDetail.IDMataKuliah,
+						&nilaiDetail.Nilai, &nilaiDetail.Semester,
+					)
+
+					sqlMataKuliah :=
+						`SELECT nama
+						FROM mata_kuliah JOIN nilai USING(id_mata_kuliah) WHERE id_mata_kuliah = ?`
+					rowsMataKuliah, _ := db.Query(sqlMataKuliah, nilaiDetail.IDMataKuliah)
+
+					// mata kuliah
+					if rowsMataKuliah.Next() {
+						rowsMataKuliah.Scan(&nilaiDetail.MataKuliah)
+					}
+
+					mahasiswa.NilaiDetail.NilaiDetailList = append(mahasiswa.NilaiDetail.NilaiDetailList, nilaiDetail)
+				}
+
+				response.Mahasiswa.MahasiswaList = append(response.Mahasiswa.MahasiswaList, mahasiswa)
+			}
+		}
+
+		w.Write([]byte(xml.Header))
+		encoder := xml.NewEncoder(w)
+		encoder.Indent("", "\t")
+		encoder.Encode(response)
+	}
+}
+
 func main() {
 	// database
-	db := getConnection()
+	db = getConnection()
 	defer db.Close()
 
 	// init router
 	router := mux.NewRouter()
 
-	// index
+	// router url
 	router.HandleFunc("/", index).Methods("GET")
-
-	// init objek mahasiswa
-	mahasiswa := m.Mahasiswa{DB: db}
-
-	// route mahasiswa
-	router.HandleFunc("/mahasiswa", mahasiswa.GetAllMahasiswa).Methods("GET")
-	router.HandleFunc("/mahasiswa/{id}", mahasiswa.GetMahasiswa).Methods("GET")
-	router.HandleFunc("/mahasiswa/{id}", mahasiswa.UpdateMahasiswa).Methods("PUT")
-	router.HandleFunc("/mahasiswa/{id}", mahasiswa.DeleteMahasiswa).Methods("DELETE")
-	router.HandleFunc("/mahasiswa", mahasiswa.InsertMahasiswa).Methods("POST")
-
-	// init object nilai
-	nilai := n.Nilai{DB: db}
-
-	// route nilai
-	router.HandleFunc("/nilai", nilai.GetAllNilai).Methods("GET")
-	router.HandleFunc("/nilai/{id}", nilai.GetNilai).Methods("GET")
-	router.HandleFunc("/nilai/{id}", nilai.UpdateNilai).Methods("PUT")
-	router.HandleFunc("/nilai/{id}", nilai.DeleteNilai).Methods("DELETE")
-	router.HandleFunc("/nilai", nilai.InsertNilai).Methods("POST")
-
-	// init object mata kuliah
-	mataKuliah := mK.MataKuliah{DB: db}
-
-	// route mata kuliah
-	router.HandleFunc("/mata-kuliah", mataKuliah.GetAllMataKuliah).Methods("GET")
-	router.HandleFunc("/mata-kuliah/{id}", mataKuliah.GetMataKuliah).Methods("GET")
-	router.HandleFunc("/mata-kuliah/{id}", mataKuliah.UpdateMataKuliah).Methods("PUT")
-	router.HandleFunc("/mata-kuliah/{id}", mataKuliah.DeleteMataKuliah).Methods("DELETE")
-	router.HandleFunc("/mata-kuliah", mataKuliah.InsertMataKuliah).Methods("POST")
+	router.HandleFunc("/nilai-mahasiswa", nilaiMahasiswa).Methods("GET")
+	router.HandleFunc("/nilai-mahasiswa/{idMahasiswa}", nilaiMahasiswa).Methods("GET")
+	router.HandleFunc("/nilai-mahasiswa/{idMahasiswa}/{idMataKuliah}", nilaiMahasiswa).Methods("GET")
 
 	// start server with port 8080
 	log.Fatal(http.ListenAndServe(":8000", router))
@@ -78,7 +180,7 @@ func getConnection() *sql.DB {
 	file, err := ioutil.ReadFile("../config.yml")
 	checkErr(err)
 
-	var config Config
+	var config config
 	err = yaml.Unmarshal(file, &config)
 	checkErr(err)
 
